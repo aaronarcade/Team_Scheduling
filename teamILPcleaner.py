6f1167738704694f4541e588f60cbbcebf0cd832
aaronarcade <AaronBBrown@gatech.edu>
2019-06-08 23:57:03 -0400

    Initial commit
    adding cleaning file, redacted response form, and general process pdf for reference

---
+++ teamILPcleaner.py
@@ -0,0 +1,45 @@
+import csv, pprint
+
+members = []
+with open('DummyApplicationResponses_preclean.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    headers = next(reader, None)
+    avg_year = 0
+    for row in reader:
+
+
+        #make dict of preferences *****be sure to change number of projects, no projects = 2nd number in 2nd param of range
+        proj_pref = {}
+        for proj in range(20, 20+9):
+            if 'VERY' in row[proj]:
+                proj_pref[headers[proj]] = 3
+            elif 'Interested' in row[proj]:
+                proj_pref[headers[proj]] = 2
+            elif 'Mildly' in row[proj]:
+                proj_pref[headers[proj]] = 1
+
+        #make year numeric
+        if 'First' in row[9]:
+            temp_year = 1
+        elif 'Second' in row[9]:
+            temp_year = 2
+        elif 'Third' in row[9]:
+            temp_year = 3
+        elif 'Fourth' in row[9]:
+            temp_year = 4
+        else:
+            temp_year = 5
+
+        #add year to total years
+        avg_year+=temp_year
+
+        #add member to list of dictionaries of all members
+        members.append({
+            'name':row[2],
+            'year':temp_year,
+            'projects':proj_pref
+            })
+pprint.pprint(members[0])
+#solve for average year among all members
+avg_year = round(avg_year/len(members),4)
+print('Average year of all members: ', avg_year)
